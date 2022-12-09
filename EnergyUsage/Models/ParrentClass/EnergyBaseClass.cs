8d17f63cc33ebe902fe669318789a16ec6abc3fd
mellow <stay.mellow.993@gmail.com>
2022-12-09 14:04:07 +0100

    Energy object add

--- EnergyUsage/Models/ParrentClass/EnergyBaseClass.cs
+++ EnergyUsage/Models/ParrentClass/EnergyBaseClass.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace EnergyUsage.Models.ParrentClass
 {
-    abstract internal class EnergyBaseClass
+    internal class Energy
     {
         public DateTime DayOfMeaurment { get; set; }
+        public double AmountOfWater { get; set; }
+        public double AmountOfGas { get; set; }
+        public double AmountOfCurrent { get; set; }
 
         enum Rooms
         {
@@ -17,6 +20,14 @@ namespace EnergyUsage.Models.ParrentClass
             Wohnzimmer,
             Keller,
             Schlafzimmer,
+        }
+
+        public Energy(double amountOfWater, double amountOfGas, double amountOfCurrent)
+        {
+            //DayOfMeaurment = dayOfMeaurment;
+            AmountOfWater = amountOfWater;
+            AmountOfGas = amountOfGas;
+            AmountOfCurrent = amountOfCurrent;
         }
     }
 }
--- EnergyUsage/ViewModels/MainWindowsViewModel.cs
+++ EnergyUsage/ViewModels/MainWindowsViewModel.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using EnergyUsage.Common;
 using EnergyUsage.Common.Interfaces;
 using EnergyUsage.Common.Repo;
+using EnergyUsage.Models.ParrentClass;
 using EnergyUsage.ViewModelBases;
 using EnergyUsage.Views;
 
@@ -14,11 +16,12 @@ namespace EnergyUsage.ViewModels
     internal class MainWindowsViewModel : ViewModelBase
     {
         private IRepository _repository;
+
         public DelegateCommand OpenCommand { get; private set; }
+        public DelegateCommand ExportCommand { get; private set; }
 
         internal MainWindowsViewModel(IRepository repository) 
         {
-            //_iView = iView;
             _repository = repository;
             SetUpCommands();
         }
@@ -26,8 +29,20 @@ namespace EnergyUsage.ViewModels
         private void SetUpCommands()
         {
             OpenCommand = new DelegateCommand(OpenInputWindow);
+            ExportCommand = new DelegateCommand(ExportData);
         }
-        
+
+        private void ExportData()
+        {
+            Energy energy = new Energy(10,10,10);
+            Export export = new Export(energy);
+            export.SaveJson();
+
+
+
+        }
+
+
         private void OpenInputWindow()
         {
             MessageBox.Show("open");
